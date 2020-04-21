c8538b0b3d96b841f38bbdd66f32f7471f4861de
rukayaj@gmail.com <rukayaj@gmail.com>
2020-04-21 14:38:50 +0200

    Better locality data

--- script/dnvmodtodwc.py
+++ script/dnvmodtodwc.py
@@ -11,27 +11,38 @@ def add_uuids(occurrence):
     occurrence['occurrenceID'] = [uuid.uuid4() for x in range(len(occurrence.index))]
     occurrence['eventID'] = occurrence.groupby('Station')['Station'].transform(lambda x: uuid.uuid4())
 
-def create_event_sheet(occurrence, stations_report, current_sea):
+def create_event_sheet(occurrence, stations_report):
     station_years = occurrence['Station'].str.split(' ', expand=True)
     event = pd.DataFrame({
         'eventID': occurrence['eventID'],
         'Station': station_years[1],
         'eventRemarks': 'grab ' + station_years[2],
         'year': station_years[0],
-        'month': '4',
-        'geodeticDatum': 'WGS84',
-        'waterBody': current_sea
+        'eventDate': station_years[0] + '-05/' + station_years[0] + '-06'
     })
     event.drop_duplicates(inplace=True)
-    event['eventDate'] = event['year'].apply(lambda x: '{}-05/{}-06'.format(x, x))
-    return pd.merge(event, stations_report, how='left', on='Station')
+    event = pd.merge(event, stations_report, how='left', on='Station')
+    return event
+
+def set_location_data(event, current_sea):
+    event['waterBody'] = current_sea
+    event['geodeticDatum'] = 'WGS84'
+    event['locationRemarks'] = 'station ' + event['Station'] + ', direction from station: ' + event['Direction'].astype(str) + ', distance from station: ' + event['Distance'].astype(str)
+    event['locationRemarks'] = event['locationRemarks'].str.replace(', direction from station: nan, distance from station: nan', '')
+    event['locationRemarks'] = event['locationRemarks'].str.replace(', direction from station: None, distance from station: None', '')
+    event['maximumDepthInMeters'] = event['Depth']
+    event.drop(columns=['Distance', 'Direction', 'UTM31E', 'UTM31N', 'UTM32E', 'UTM32N', 'UTM34E', 'UTM34N', 'UTM35E', 'UTM35N', 'UTM36E', 'UTM36N', 'ED50E', 'ED50N'], inplace=True)
+
+    event['verbatimCoordinateSystem'] = 'UTM'
+    event['verbatimSrS'] = 'EPSG:32633'
+    event.rename(columns={'Installation': 'locality', 'Depth': 'minimumDepthInMeters', 'WGS84E': 'decimalLongitude', 'WGS84N': 'decimalLatitude', 'UTM33E': 'verbatimLongitude', 'UTM33N': 'verbatimLatitude'}, inplace=True)
+    event.loc[pd.isnull(event['decimalLatitude']), 'geodeticDatum'] = 'EPSG:32633'
+    event.loc[pd.isnull(event['decimalLongitude']), 'decimalLongitude'] = event.loc[pd.isnull(event['decimalLongitude']), 'verbatimLongitude']
+    event.loc[pd.isnull(event['decimalLatitude']), 'decimalLatitude'] = event.loc[pd.isnull(event['decimalLatitude']), 'verbatimLatitude']
+    return event
 
 def dwcify_columns(occurrence, event):
     occurrence['basisOfRecord'] = 'MaterialSample'
     occurrence.rename(columns={'Species': 'scientificName', 'Family': 'family'}, inplace=True)
     #occurrence.drop(columns='Station', inplace=True)
-    event['locationRemarks'] = 'station ' + event['Station'] + ', direction from station: ' + event['Direction'].astype(str) + ', distance from station: ' + event['Distance'].astype(str)
-    #event.drop(columns=['Station', 'Direction', 'Distance'], inplace=True)
-    event['maximumDepthInMeters'] = event['Depth']
-    event.rename(columns={'Installation': 'locality', 'Depth': 'minimumDepthInMeters', 'WGS84E': 'decimalLongitude', 'WGS84N': 'decimalLatitude'}, inplace=True)
 
--- script/tests.py
+++ script/tests.py
@@ -1,28 +1,88 @@
 import unittest
-from dnvmodtodwc import reverse_occurrence_pivot, add_uuids, create_event_sheet, dwcify_columns
+from dnvmodtodwc import reverse_occurrence_pivot, add_uuids, create_event_sheet, dwcify_columns, set_location_data
 import pandas as pd
 import numpy as np
 import uuid
 
 class TestCreateEventSheet(unittest.TestCase):
-    def test_creates_event_sheet(self):
+    def test_creates_a_single_record_per_event_with_correct_dates_and_grab_numbers(self):
         occurrence = pd.DataFrame({'Station': ['2008 R10-1 1', '2008 R10-1 1', '2008 R10-1 2', '2010 NV9 5'],
                                    'eventID': [10, 10, 11, 12],
                                    'Species': ['A', 'B', 'A', 'A']})
-        stations_report = pd.DataFrame({'Station': ['R10-1', 'NV9'],
-                                        'decimalLatitude': [70, 80],
-                                        'decimalLongitude': [20, 30]})
-        events = create_event_sheet(occurrence, stations_report, 'Barents Sea South')
+        stations_report = pd.DataFrame({'Station': ['R10-1', 'NV9']})
+        events = create_event_sheet(occurrence, stations_report)
         expected = pd.DataFrame({'eventID': [10, 11, 12],
                                  'Station': ['R10-1', 'R10-1', 'NV9'],
                                  'eventRemarks': ['grab 1', 'grab 2', 'grab 5'],
                                  'year': ['2008', '2008', '2010'],
-                                 'eventDate': ['2008-05/2008-06', '2008-05/2008-06', '2010-05/2010-06']
-                                 'geodeticDatum': 'WGS84',
-                                 'waterBody': 'Barents Sea South',
-                                 'decimalLatitude': [70, 70, 80],
-                                 'decimalLongitude': [20, 20, 30]})
+                                 'eventDate': ['2008-05/2008-06', '2008-05/2008-06', '2010-05/2010-06']})
         np.testing.assert_array_equal(events.values, expected.values)
+
+
+class TestSetLocationData(unittest.TestCase):
+    def test_adds_correct_geodetic_datums_when_wgs84_unavailable(self):
+        event = pd.DataFrame({'Station': ['J1', 'J2', 'J3'],
+                              'Depth': [100, 101, 102],
+                              'Direction': [30, 60, 90],
+                              'Distance': [1, 2, 3],
+                              'Installation': ['Jeba', 'Jeba', 'Jeba'],
+                              'WGS84N': [70, None, None],
+                              'WGS84E': [20, None, None],
+                              'UTM33N': [8000000, 8000001, 8000002],
+                              'UTM33E': [730000, 730001, 730002],
+                              'UTM31E': '', 'UTM31N': '', 'UTM32E': '', 'UTM32N': '', 'UTM34E': '', 'UTM34N': '',
+                              'UTM35E': '', 'UTM35N': '', 'UTM36E': '', 'UTM36N': '', 'ED50E': '', 'ED50N': ''
+                              })
+        events = set_location_data(event, 'UK Shelf')
+        expected = pd.DataFrame({'Station': ['J1', 'J2', 'J3'],
+                                 'geodeticDatum': ['WGS84', 'EPSG:32633', 'EPSG:32633'],
+                                 'waterBody': ['UK Shelf', 'UK Shelf', 'UK Shelf'],
+                                 'decimalLatitude': [70, 8000001, 8000002],
+                                 'decimalLongitude': [20, 730001, 730002],
+                                 'verbatimLatitude': [8000000, 8000001, 8000002],
+                                 'verbatimLongitude': [730000, 730001, 730002],
+                                 'verbatimSrS': ['EPSG:32633', 'EPSG:32633', 'EPSG:32633'],
+                                 'verbatimCoordinateSystem': ['UTM', 'UTM', 'UTM'],
+                                 'maximumDepthInMeters': [100, 101, 102],
+                                 'minimumDepthInMeters': [100, 101, 102],
+                                 'locality': ['Jeba', 'Jeba', 'Jeba'],
+                                 'locationRemarks': ['station J1, direction from station: 30, distance from station: 1',
+                                                     'station J2, direction from station: 60, distance from station: 2',
+                                                     'station J3, direction from station: 90, distance from station: 3']
+                                 })
+        pd._testing.assert_frame_equal(events.reset_index(), expected.reset_index(), check_like=True, check_dtype=False)
+
+    def test_creates_sensible_location_remarks(self):
+        event = pd.DataFrame({'Station': ['J1', 'J2', 'J3'],
+                              'Depth': [100, 101, 102],
+                              'Direction': pd.Series([30, None, 90], dtype=object),
+                              'Distance': pd.Series([1, None, 3], dtype=object),
+                              'Installation': ['Jeba', 'Jeba', 'Jeba'],
+                              'WGS84N': [70, None, None],
+                              'WGS84E': [20, None, None],
+                              'UTM33N': [8000000, 8000001, 8000002],
+                              'UTM33E': [730000, 730001, 730002],
+                              'UTM31E': '', 'UTM31N': '', 'UTM32E': '', 'UTM32N': '', 'UTM34E': '', 'UTM34N': '',
+                              'UTM35E': '', 'UTM35N': '', 'UTM36E': '', 'UTM36N': '', 'ED50E': '', 'ED50N': ''
+                              })
+        events = set_location_data(event, 'UK Shelf')
+        expected = pd.DataFrame({'Station': ['J1', 'J2', 'J3'],
+                                 'geodeticDatum': ['WGS84', 'EPSG:32633', 'EPSG:32633'],
+                                 'waterBody': ['UK Shelf', 'UK Shelf', 'UK Shelf'],
+                                 'decimalLatitude': [70, 8000001, 8000002],
+                                 'decimalLongitude': [20, 730001, 730002],
+                                 'verbatimLatitude': [8000000, 8000001, 8000002],
+                                 'verbatimLongitude': [730000, 730001, 730002],
+                                 'verbatimSrS': ['EPSG:32633', 'EPSG:32633', 'EPSG:32633'],
+                                 'verbatimCoordinateSystem': ['UTM', 'UTM', 'UTM'],
+                                 'maximumDepthInMeters': [100, 101, 102],
+                                 'minimumDepthInMeters': [100, 101, 102],
+                                 'locality': ['Jeba', 'Jeba', 'Jeba'],
+                                 'locationRemarks': ['station J1, direction from station: 30, distance from station: 1',
+                                                     'station J2',
+                                                     'station J3, direction from station: 90, distance from station: 3']
+                                 })
+        pd._testing.assert_frame_equal(events.reset_index(), expected.reset_index(), check_like=True, check_dtype=False)
 
 
 class TestReverseOccurrencePivot(unittest.TestCase):
